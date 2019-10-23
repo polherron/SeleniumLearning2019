6a20afaf8af7a81cd2df481f8f6e98addbd0ed16
polherron <pol.herron@gmail.com>
2019-10-23 16:32:05 +0100

    Find Elements Using WebElement

--- SeleniumWebDriverLearning/src/main/java/com/SeleniumTesting/FisrtSeleniumTest.java
+++ SeleniumWebDriverLearning/src/main/java/com/SeleniumTesting/FisrtSeleniumTest.java
@@ -1,34 +1,45 @@
 package com.SeleniumTesting;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FisrtSeleniumTest {
 
 	public static void main(String[] args) {
-		callGeckoDriver();
-		callChromeDriver();
+		doSearch(("cars"));
 	}
 
 	/**
 	 * Calls a chrome driver and opens 
 	 * the browser at google.com
+	 * then does a search based on the name of
+	 * the search text box.
+	 *  
 	 */
-	private static void callChromeDriver() {
-		System.setProperty("webdriver.chrome.driver", "c:/dev/tools/chromedriver.exe");
+	private static void doSearch(String searchString) {
+		//System.setProperty("webdriver.chrome.driver", "c:/dev/tools/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+
 		driver.get("https://www.google.com/");
+		
+		//Get a web element
+		WebElement searchField = driver.findElement(By.name("q"));
+		
+		//Pass a string to the text box element
+		searchField.sendKeys(searchString);
+		
+		//Find the search button
+		WebElement searchButton = driver.findElement(By.name("btnK"));
+		
+		//Click the button
+		searchButton.click();
+		
+		//This would have done the same thing
+		//searchButton.submit();
 	}
 
-	/**
-	 * Calls a Gecko (firefox) driver and opens 
-	 * the browser at pluralsight.com
-	 */
-	private static void callGeckoDriver() {
-		System.setProperty("webdriver.gecko.driver", "c:/dev/tools/geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
-		driver.get("http://www.pluralsight.com/");
-	}
 }
 
