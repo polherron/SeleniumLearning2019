2e3906862d007e7c52aff1b72ddb708e9c8a679a
polherron <pol.herron@gmail.com>
2019-10-25 10:28:14 +0100

    Fluent Wait code added

--- SeleniumWebDriverLearning/src/main/java/com/SeleniumTesting/FisrtSeleniumTest.java
+++ SeleniumWebDriverLearning/src/main/java/com/SeleniumTesting/FisrtSeleniumTest.java
@@ -1,7 +1,11 @@
 package com.SeleniumTesting;
 
+import java.util.NoSuchElementException;
+import java.util.function.Function;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -23,12 +27,32 @@ public class FisrtSeleniumTest {
 	 */
 	private static void clickOnImage(WebDriver driver) {
 			
-		//We set an explicit wait
-		WebDriverWait wait = new WebDriverWait(driver,30);
-	    WebElement element = wait.until(
-	    		ExpectedConditions.elementToBeClickable(By.id("ocRp6IDmQhQL3M:")));
-	    element.click();
+		//Declaring a fluent wait with the timeout of 
+		//30 seconds and the frequency is set to 
+		//5 seconds by ignoring "NoSuchElementException
+		//Frequency is set to 5 seconds and the maximum time is set 
+	 	//30 seconds
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)							
+				.withTimeout(30, TimeUnit.SECONDS) 			
+				.pollingEvery(5, TimeUnit.SECONDS) 			
+				.ignoring(NoSuchElementException.class);
+		
+		
+			//Here we are using a java Function Interface.  
+			//See: https://www.geeksforgeeks.org/function-interface-in-java-with-examples/
+		
+			WebElement image = wait.until(new Function<WebDriver, WebElement>(){
+		
+			public WebElement apply(WebDriver driver ) {
+				return driver.findElement(By.id("ocRp6IDmQhQL3M:"));
+			}
+		});
+		//click on the image
+		image.click();
+		//close~ browser
+		//driver.close() ;
 
+		
 	}
 	
 	/**
@@ -42,7 +66,9 @@ public class FisrtSeleniumTest {
 	}
 
 	/**
-	 * Calls a chrome driver and opens 
+	 * Calls a chrome dri
+	 * 
+	 * ver and opens 
 	 * the browser at google.com
 	 * then does a search based on the name of
 	 * the search text box.
@@ -60,14 +86,9 @@ public class FisrtSeleniumTest {
 		//Pass a string to the text box element
 		searchField.sendKeys(searchString);
 		
-		//Find the search button
-		WebElement searchButton = driver.findElement(By.name("btnK"));
-		
-		//Click the button
-		searchButton.click();
 		
 		//This would have done the same thing
-		//searchButton.submit();
+		searchField.submit();
 	}
 
 }
